f36d1be5eccf4029eb0651369034617590c6f178
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-07-20 21:45:43 +0100

    added buttombar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,39 @@
 /**
  * Layout component that queries for data
  * with Gatsby's useStaticQuery component
- *
+ * import { FaBeer } from 'react-icons/fa';
+
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import { MdMessage, MdPerson, MdTrendingUp, MdAdd, MdHome } from "react-icons/md";
+import { IconContext } from "react-icons";
 
-import Header from "./header"
+
+
+// import Header from "./header"
 import "./layout.css"
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    position: 'fixed',
+    bottom: 0,
+    // maxWidth: '200px'
+
+  },
+  centerButton: {
+    backgroundColor: "lightblue"
+  }
+});
+
+
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -22,24 +45,33 @@ const Layout = ({ children }) => {
       }
     }
   `)
-
+  console.log(data);
+  const classes = useStyles();
+  const [navvalue, setNavvalue] = React.useState('recents');
   return (
+
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div>
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+
       </div>
+      <BottomNavigation
+        value={navvalue}
+        onChange={(event, newValue) => {
+          setNavvalue(newValue);
+        }}
+        showLabels
+        className={classes.root}
+      >
+        <BottomNavigationAction label="Home" icon={<MdHome />} />
+        <BottomNavigationAction label="Trending" icon={<MdTrendingUp />} onClick={() => { console.log("Fav") }} />
+        {/* <BottomNavigationAction className={classes.centerButton} icon={<WhiteLargeIcon />} /> */}
+        <BottomNavigationAction label="Messages" icon={<MdMessage />} />
+        <BottomNavigationAction label="Profile" icon={<MdPerson />} />
+      </BottomNavigation>
+      <footer>
+        nav here
+      </footer>
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,36 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
 
+// function getdata() {
+//   console.log("here");
+
+//   const url = 'https://randomuser.me/api';
+//   // The data we are going to send in our request
+
+//   fetch(url)
+//     .then(function (response) {
+//       // Handle response we get from the API
+//       console.log(response);
+
+//     })
+// }
 const IndexPage = () => (
+
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    {/* <button onClick={getdata}> getdata</button> */}
+    {/* <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
   </Layout>
 )
 
