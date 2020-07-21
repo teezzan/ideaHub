94d2778eea1e796c4c588085a3d0ba07eb97acf5
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-07-21 22:56:45 +0100

    added client side graphql fetching

--- src/apolloFiles/client.js
+++ src/apolloFiles/client.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 import fetch from 'isomorphic-fetch';
 
 export const client = new ApolloClient({
-  uri: 'https://api-euwest.graphcms.com/v1/cjke2kn7p00ol01d2pinkptdj/master',
+  uri: 'http://localhost:4000/graphql',
   cache: new InMemoryCache(),
   fetch,
 });
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,52 +10,32 @@ import { useQuery, gql } from '@apollo/client';
 // This query is executed at run time by Apollo.
 const APOLLO_QUERY = gql`
   {
-    meme(where: { id: "cjke2xlf9nhd90953khilyzja" }) {
-      photo {
-        url(
-          transformation: {
-            image: { resize: { width: 600, height: 600, fit: crop } }
-          }
-        )
+    userbyusername(username:"1234"){
+    status
+    user{
+      username  
       }
-    }
+  }
   }
 `;
 
 
-// function getdata() {
-//   console.log("here");
 
-//   const url = 'https://randomuser.me/api';
-//   // The data we are going to send in our request
-
-//   fetch(url)
-//     .then(function (response) {
-//       // Handle response we get from the API
-//       console.log(response);
-
-//     })
-// }
-const IndexPage = ({
-  data: {
-    rickAndMorty: { character },
-  },
-}) => {
-  const { loading, error, data } = useQuery(APOLLO_QUERY);
+const IndexPage = () => {
+  const { data } = useQuery(APOLLO_QUERY);
+  console.log(data);
+  var a = 555;
+  console.log(a === a.length);
   return (
     <Layout>
       <SEO title="Home" />
       {/* <button onClick={getdata}> getdata</button> */}
 
-      {loading && <p>Loading Sara...</p>}
-      {error && <p>Error: ${error.message}</p>}
-      {data && data.meme && data.meme.photo && (
-        <img
-          src={data.meme.photo.url}
-          alt="Sara Vieira"
-          style={{ maxWidth: 300 }}
-        />
-      )}
+
+      {/* {error && <p>Error: ${error.message}</p>}
+      {data && (
+        <p> {JSON.stringify(data.userbyusername)}</p>
+      )} */}
 
 
       <h1>Hi people</h1>
