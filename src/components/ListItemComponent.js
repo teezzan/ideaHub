b595e2c1ecbe19d0c17e90dd5d76ae9a8408cccf
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-08-04 17:23:59 +0100

    added like button . TBD reduce size and change color

--- src/components/ListItemComponent.js
+++ src/components/ListItemComponent.js
@@ -5,6 +5,9 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import { FaHeart } from "react-icons/fa";
+import { ListItemSecondaryAction, IconButton } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
 
@@ -18,27 +21,36 @@ export default function ListItemComp() {
     const classes = useStyles();
 
     return (
+        <React.Fragment>
 
-        <ListItem button alignItems="flex-start">
-            <ListItemAvatar>
-                <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-            </ListItemAvatar>
-            <ListItemText
-                primary="Oui Oui"
-                secondary={
-                    <React.Fragment>
-                        <Typography
-                            component="span"
-                            variant="body2"
-                            className={classes.inline}
-                            color="textPrimary"
-                        >
-                            Sandra Adams
-              </Typography>
-                        {' — I llbe in your neighborhood doing errands this   Do you have Paris recommendations? Have you ever…'}
-                    </React.Fragment>
-                }
-            />
-        </ListItem>
+            <ListItem button alignItems="flex-start">
+                <ListItemAvatar>
+                    <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+                </ListItemAvatar>
+                <ListItemText
+                    primary="Oui Oui"
+                    secondary={
+                        <React.Fragment>
+                            <Typography
+                                component="span"
+                                variant="body2"
+                                className={classes.inline}
+                                color="textPrimary"
+                            >
+                                Sandra Adams
+                  </Typography>
+                            {' — I llbe in your neighborhood doing errands this   Do you have Paris recommendations? Have you ever…'}
+                        </React.Fragment>
+                    }
+                />
+
+                <ListItemSecondaryAction>
+                    <IconButton edge="end" aria-label="like">
+                        <FaHeart />
+                    </IconButton>
+                </ListItemSecondaryAction>
+            </ListItem>
+            <Divider variant="inset" component="li" />
+        </React.Fragment>
     );
 }
--- src/components/UserIdeaComponent.js
+++ src/components/UserIdeaComponent.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
 import ListItemComp from './ListItemComponent';
 
 const useStyles = makeStyles((theme) => ({
@@ -31,7 +25,6 @@ export default function UserIdeaComponent() {
     return (
         <List className={classes.root}>
             <ListItemComp />
-            <Divider variant="inset" component="li" />
             <ListItemComp />
         </List>
     );
